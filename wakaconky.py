05165077a11a52607f45fd59a69639cb0d08c2e8
thassiov <tvmcarvalho@gmail.com>
2017-08-07 16:19:28 -0300

    Adding 'makeNewAccessToken' (code from wakatime's docs)

--- wakaconky.py
+++ wakaconky.py
@@ -26,3 +26,33 @@ def getAccessTokenFromFile():
             return token
     return ''
 
+def makeNewAccessToken():
+    """yeah. code from [https://wakatime.com/developers#authentication]
+    :returns: string
+
+    """
+    client_id = input('Enter your App Id: ')
+    secret = input('Enter your App Secret: ')
+
+    service = OAuth2Service(
+        client_id=client_id, # your App ID from https://wakatime.com/apps
+        client_secret=secret, # your App Secret from https://wakatime.com/apps
+        name='wakatime',
+        authorize_url='https://wakatime.com/oauth/authorize',
+        access_token_url='https://wakatime.com/oauth/token',
+        base_url='https://wakatime.com/api/v1/')
+
+    redirect_uri = 'https://wakatime.com/oauth/test'
+    state = hashlib.sha1(os.urandom(40)).hexdigest()
+    params = {'scope': 'email,read_stats',
+              'response_type': 'code',
+              'state': state,
+              'redirect_uri': redirect_uri}
+
+    url = service.get_authorize_url(**params)
+
+    print('**** Visit {url} in your browser. ****'.format(url=url))
+    print('**** After clicking Authorize, paste code here and press Enter ****')
+    code = input('Enter code from url: ')
+    return code
+
