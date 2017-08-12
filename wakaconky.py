1ca4b0ca124844fa940b2aa48e64de4a5c589dc5
thassiov <tvmcarvalho@gmail.com>
2017-08-12 10:27:29 -0300

    try not to headband https://www.youtube.com/watch\?v\=HpRiJWumGas

--- wakaconky.py
+++ wakaconky.py
@@ -35,34 +35,88 @@ def getSummary(token):
     today = datetime.datetime.now()
     startTime = today.strftime('%Y-%m-%d')
     params = {'start':startTime, 'end':startTime, 'api_key':token} 
+    route = 'https://wakatime.com/api/v1/users/current/summaries'
+    return callWakatimeAPI(params, route)
 
-    headers = {'Accept':'application/x-www-form-urlencoded'}
-    api_route = 'https://wakatime.com/api/v1/users/current/summaries'
-    r = requests.get(api_route, headers=headers, params=params)
-    return r.json()
+def getStats(token):
+    """get the user's stats for the last 7 days
 
-def storeWakatimeData(data):
-    """stores writes the data in '~/wakaconky' so conky can read it
-    :returns: TODO
+    :token: string
+    :returns: json
 
     """
+    timeRange = 'last_7_days'
+    params = {'api_key':token} 
+    route = 'https://wakatime.com/api/v1/users/current/stats/' + timeRange
+    return callWakatimeAPI(params, route)
+
+def callWakatimeAPI(params, route):
+    """handles the API requests
+
+    :params: object
+    :route: string
+    :returns: json
+
+    """
+    headers = {'Accept':'application/x-www-form-urlencoded'}
+    r = requests.get(route, headers=headers, params=params)
+    return r.json()
+
+def getWakatimeData(token):
+
+    toBeStored = []
+
+    summary = getSummary(token)
+    toBeStored.append('time_spent_today = ' + summary["data"][0]["grand_total"]["text"]) # total
+    minutes = int(summary["data"][0]["grand_total"]["total_seconds"])/60
+    percent = minutes/(480/100) # 480 minutes = 8 hours
+    percent = "{0:.2f}".format(percent)
+    toBeStored.append('time_spent_today_as_percentage = ' + percent)
+
+    last7days = getStats(token)
+    bestDayInMinutes = int(last7days["data"]["best_day"]["total_seconds"])/60
+    bestDayInMinutes = "{0:.2f}".format(bestDayInMinutes)
+    bestDay = str(last7days["data"]["best_day"]["date"]) + ' - ' + str(bestDayInMinutes)
+    toBeStored.append('best_day = ' + bestDay + ' mins')
+
+    weekLang = last7days["data"]["languages"][0]["name"]
+    weekLang += ' - ' + last7days["data"]["languages"][0]["text"]
+    toBeStored.append("lang_of_the_week = " + weekLang)
+
+    weekProj = last7days["data"]["projects"][0]["name"]
+    weekProj += ' - ' + last7days["data"]["projects"][0]["text"]
+    toBeStored.append("project_of_the_week = " + weekProj)
+
+    for item in toBeStored:
+        appendToWakaconkyData(item)
+
+def wipeOldData():
+    """erases wakaconky.data's content
+    :returns: boolean
+
+    """
+    wkdata = os.path.expanduser('~/.wakaconky.data')
+    try:
+        open(wkdata, 'w').close()
+    except (OSError, IOError) as e:
+        print(e)
+        return False
+    return True
+
+def appendToWakaconkyData(toAppend):
     dataFile = os.path.expanduser('~/.wakaconky.data')
     try:
-        f = open(dataFile, 'w')
+        f = open(dataFile, 'a')
     except Exception as e:
         raise e
-
-    data = getSummary(token)
-    f.write(data["data"][0]["grand_total"]["text"]) # total
-    minutes = int(data["data"][0]["grand_total"]["total_seconds"])/60
-    percent = minutes/(480/100)
-    f.write('\n')
-    percent = "{0:.2f}".format(percent)
-    f.write(str(percent))
+    for dataChunk in toAppend:
+        f.write(dataChunk)
     f.close()
+    return True
 
 token = getAccessTokenFromFile()
 if token == '':
     print('api key needed!')
 else:
-    storeWakatimeData(getSummary(token))
+    wipeOldData()
+    getWakatimeData(token)
